2456bc47f2bdf7653b31d9e767a1a9b83e428da1
clouddevopsguru123 <149225143+clouddevopsguru123@users.noreply.github.com>
2023-10-28 11:44:35 +0530

    Create user_input_example4.sh

---
+++ user_input_example4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Author: ram
+#Discription: this is for install the httpd package
+#Date creation: 15/10/23
+#Date of modify:
+
+read -p "Enter user name " name
+read -sp "Enter user password " passwd
+
+echo "user name is ${name} and password is ${passwd}
