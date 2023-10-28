f689f7e2ab7716628f64035e4b711ad7ebbd5f3f
clouddevopsguru123 <149225143+clouddevopsguru123@users.noreply.github.com>
2023-10-28 11:47:12 +0530

    Create arguments_with_array.sh

---
+++ arguments_with_array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Author: ram
+#Discription: Display the hello world message
+#Date creation: 24/10/23
+#Date of modify: 24/10/23
+
+#echo Hello World
+#echo $1 $2
+
+args=("$@")
+
+echo $@
