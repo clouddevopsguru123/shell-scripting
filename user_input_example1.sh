d4c611698c9707e716260f2391ae3076b91871f3
clouddevopsguru123 <149225143+clouddevopsguru123@users.noreply.github.com>
2023-10-28 11:42:36 +0530

    Create user_input_example1.sh

---
+++ user_input_example1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Author: ram
+#Discription: this is for install the httpd package
+#Date creation: 19/10/23
+#Date of modify: 19/10/23
+
+echo "please insert the user name"
+read name
+echo "please insert the age "
+read age
+echo "please insert the city name"
+read city
+
+
+echo "Welcome to our shell script practice ${name} , ${name}, ${age} , ${city} "
