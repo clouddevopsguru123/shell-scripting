f22c990c3ccbda401a2f4d675c44e60c64da614b
clouddevopsguru123 <149225143+clouddevopsguru123@users.noreply.github.com>
2023-10-28 11:36:23 +0530

    Create Basic_shell_script.sh

---
+++ Basic_shell_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Author: Ram
+# Discription: this script is used for display the string in terminal
+# Date of created: 14/10/23
+# Date of modify: 14/10/23
+
+ echo hello world
+ 
+ pwd
+ ls
+ touch file2 file3
+ ls
