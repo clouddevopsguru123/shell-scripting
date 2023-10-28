def7f805f4ee0a42b6bb4887401c9cb93bd6b636
clouddevopsguru123 <149225143+clouddevopsguru123@users.noreply.github.com>
2023-10-28 11:45:41 +0530

    Create passing_arguments.sh

---
+++ passing_arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Author: ram
+#Discription: Display the hello world message
+#Date creation: 24/10/23
+#Date of modify: 24/10/23
+
+sudo apt update -y
+sudo apt -y install $1 $2
+#sudo apt install $2
