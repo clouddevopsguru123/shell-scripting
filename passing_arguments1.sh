c11daf1506c6c8897b97d7c10d0811be00ad95f9
clouddevopsguru123 <149225143+clouddevopsguru123@users.noreply.github.com>
2023-10-28 11:46:24 +0530

    Create passing_arguments1.sh

---
+++ passing_arguments1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Author: ram
+#Discription: Display the hello world message
+#Date creation: 24/10/23
+#Date of modify: 24/10/23
+
+Name=$1
+age=$2
+
+echo "this is $Name"
+echo "$Name your are  $age old" 
+
+echo $#
